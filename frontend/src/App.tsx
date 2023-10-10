55b91605a82b090ad544aed842baded90dc18073
talentlessDeveloper <kareemope52@gmail.com>
2023-10-10 11:53:24 +0100

    setup frontend with vite,typescript eslint,eslint-airbnb and tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div className="my-10 flex flex-col items-center gap-2 bg-orange-300 px-5 py-28">
+        <h1 className="text-center text-3xl font-bold ">
+          Welcome to Co.lab MH App
+        </h1>
+        <hr className="max-w-md bg-black " />
+      </div>
+    </>
+  );
+}
+
+export default App;
