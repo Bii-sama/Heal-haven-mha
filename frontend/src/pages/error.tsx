1b843a37a2d15e3454b609cdf97dc7eed6cbfafc
talentlessDeveloper <kareemope52@gmail.com>
2023-10-12 12:54:31 +0100

    created pages folder,included error-page

---
+++ frontend/src/pages/error.tsx
@@ -0,0 +1,29 @@
+import { useNavigate, useRouteError } from 'react-router-dom';
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  const navigate = useNavigate();
+
+  return (
+    <div
+      id="error-page"
+      className="flex h-screen flex-col items-center justify-center"
+    >
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>
+          {(error as { statusText?: string })?.statusText ||
+            (error as Error)?.message}
+        </i>
+      </p>
+      <button
+        className="mt-1 bg-violet-800 px-6 py-1 text-lg font-bold text-white hover:bg-violet-950"
+        type="button"
+        onClick={() => navigate(-1)}
+      >
+        Go Back
+      </button>
+    </div>
+  );
+}
