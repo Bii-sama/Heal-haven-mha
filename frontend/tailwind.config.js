1f6d8b98ed052814271563a141185d78b42b9f9c
talentlessDeveloper <kareemope52@gmail.com>
2023-10-13 12:41:18 +0100

    colors and fontfamily setup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        healHavenBrand600: '#7F56D9',
+        healHavenBrand700: '#6941C6',
+        healHavenBrand800: '#53389E',
+        healHavenBrand900: '#42307D',
+        healHavenGray500: '#667085',
+        healHavenGray700: '#344054',
+        healHavenGray800: '#1D2939',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
