26eab738eb9739b0d52b2c76fcc0ba5b5c75bbd8
talentlessDeveloper <kareemope52@gmail.com>
2023-10-13 12:51:23 +0100

    routes index

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,10 +1,29 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
+import ErrorPage from '../pages/error';
+import Layout from '../components/layout';
+import Therapists from '../pages/therapists';
+import Therapist from '../pages/therapist';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <App />,
+      },
+      {
+        path: 'therapists',
+        element: <Therapists />,
+      },
+      {
+        path: 'therapist/:therapistId',
+        element: <Therapist />,
+      },
+    ],
   },
 ]);
 
