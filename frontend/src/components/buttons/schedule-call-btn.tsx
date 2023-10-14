db63a10be9f12a51795d138aa60a744b69fc2f09
talentlessDeveloper <kareemope52@gmail.com>
2023-10-14 06:32:42 +0100

    schedule call button

---
+++ frontend/src/components/buttons/schedule-call-btn.tsx
@@ -0,0 +1,14 @@
+function ScheduleCallBtn() {
+  return (
+    <div className="w-full max-w-sm lg:max-w-[190px]">
+      <button
+        type="button"
+        className="flex  w-full items-center justify-center rounded-lg bg-healHavenBrand600 px-5 py-3 text-white hover:bg-healHavenBrand900"
+      >
+        Schedule A Call
+      </button>
+    </div>
+  );
+}
+
+export default ScheduleCallBtn;
