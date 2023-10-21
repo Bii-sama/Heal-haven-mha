8c368cf6cd284ec2b62a3f2c9536a8ea983aba56
talentlessDeveloper <kareemope52@gmail.com>
2023-10-21 15:48:46 +0100

    rules form control

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -47,6 +47,24 @@ module.exports = {
   },
 
   rules: {
+    rules: {
+      'jsx-a11y/label-has-associated-control': [
+        'error',
+        {
+          required: {
+            some: ['nesting', 'id'],
+          },
+        },
+      ],
+      'jsx-a11y/label-has-for': [
+        'error',
+        {
+          required: {
+            some: ['nesting', 'id'],
+          },
+        },
+      ],
+    },
     'import/extensions': [
       'error',
       'ignorePackages',
