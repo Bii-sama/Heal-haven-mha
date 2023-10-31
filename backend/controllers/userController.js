dc5c0704f44027a0ff637dbf6a1e41bc2ca43abe
Abidemi Omosanya <89324079+Bii-sama@users.noreply.github.com>
2023-10-31 22:22:08 +0100

    email verification

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,9 +2,12 @@ const User =  require('../models/userModel');
 const mongoose = require('mongoose')
 const { signUpschema, options } = require('../utils/validation')
 const jwt = require('jsonwebtoken')
+const crypto = require('crypto')
+const sendEmail = require('./sendEmail')
 
-const createToken = (_id) => {
-  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '1d' })
+const createToken = (_id, email) => {
+  const verificationToken = crypto.randomBytes(32).toString('hex')
+  return jwt.sign({ _id, email, verificationToken }, process.env.SECRET, { expiresIn: '1d' })
 }
 
 
@@ -26,13 +29,19 @@ const signUpUser = async (req, res) =>{
     try {
         const user = await User.signup( fullName, email, password )
 
-        const token = createToken(user._id)
+        const token = createToken(user._id, user.email)
+
+        const url = `${process.env.BASE_URL}api/users/${user._id}/${token}`
+
+        sendEmail(user.email, "Verify your email", `Kindly verify here ${url}`)
 
         res.status(200).json({email, token})
     } catch (error) {
         res.status(400).json({error: error.message})
     }
 }
+
+
 
 
 //Login User
