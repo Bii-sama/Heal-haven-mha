beb136b734c0affc4bfed92eab6778ac7b20190f
Abidemi Omosanya <89324079+Bii-sama@users.noreply.github.com>
2023-11-06 09:41:57 +0100

    updateddddddddddd

--- backend/middlewares/reqAuth.js
+++ backend/middlewares/reqAuth.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const jwt = require('jsonwebtoken')
 const User =  require('../models/userModel');
-const { options } = require('joi');
 const secretKey = process.env.SECRET;
 
 
